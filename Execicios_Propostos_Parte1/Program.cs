0e52e8b00154fefdf1abeca24f4f690988a8f983
Silas Salema <silazdasilva20@gmail.com>
2019-12-20 10:27:21 -0300

    Exercicios de Calculos

---
+++ Execicios_Propostos_Parte1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Globalization;
+
+namespace Execicios_Propostos_Parte1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int resultado = a + b;
+            Console.WriteLine("Soma= " + resultado);
+
+
+            Console.WriteLine("Entre com o valor do Raio:");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double pi = 3.14159;
+            double area = pi * (Math.Pow(raio, 2));
+            Console.WriteLine("A = " + area.ToString("F4", CultureInfo.InvariantCulture));
+            -----------------------------------------------------
+            int A = int.Parse(Console.ReadLine());
+            int B = int.Parse(Console.ReadLine());
+            int C = int.Parse(Console.ReadLine());
+            int D = int.Parse(Console.ReadLine());
+            double resultado = A * B - C * D;
+            Console.WriteLine("Diferença = " + resultado);
+            -----------------------------------------------------
+
+            int numero = int.Parse(Console.ReadLine());
+            int horas = int.Parse(Console.ReadLine());
+            double valor = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double salario = valor * horas;
+            Console.WriteLine("Number = " + numero);
+            Console.WriteLine("Salary = U$ " + salario.ToString("F2", CultureInfo.InvariantCulture));
+            ------------------------------------------
+
+            string[] vetor1 = Console.ReadLine().Split(' ');
+            int codigo1 = int.Parse(vetor1[0]);
+            int peca1 = int.Parse(vetor1[1]);
+            double valorUnitario1 = double.Parse(vetor1[2], CultureInfo.InvariantCulture);
+            double total1 = peca1 * valorUnitario1;
+            
+            string[] vetor2 = Console.ReadLine().Split(' ');
+            int codigo2 = int.Parse(vetor2[0]);
+            int peca2 = int.Parse(vetor2[1]);
+            double valorUnitario2 = double.Parse(vetor2[2], CultureInfo.InvariantCulture);
+            double total2 = peca2 * valorUnitario2;
+
+            double totalPagar = total1 + total2;
+            Console.WriteLine("Valor a pagar: R$" + totalPagar.ToString("F2", CultureInfo.InvariantCulture));
+
+            --------------------------------------------------
+            string[] vetor = Console.ReadLine().Split(' ');
+            double A = double.Parse(vetor[0], CultureInfo.InvariantCulture);
+            double B = double.Parse(vetor[1], CultureInfo.InvariantCulture);
+            double C = double.Parse(vetor[2], CultureInfo.InvariantCulture);
+
+            double triangulo = (A * C) / 2;
+
+            double circulo = 3.14159 * Math.Pow(C, 2);
+
+            double trapezio = ((B + A) * C) / 2;
+
+            double quadrado = B * B;
+
+            double retangulo = A * B;
+
+            Console.WriteLine("TRIANGULO: " + triangulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("CIRCULO: " + circulo.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("TRAPEZIO: " + trapezio.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("QUADRADO: " + quadrado.ToString("F3", CultureInfo.InvariantCulture));
+            Console.WriteLine("RETANGULO: " + retangulo.ToString("F3", CultureInfo.InvariantCulture));
+            */
+        }
+    }
+}
